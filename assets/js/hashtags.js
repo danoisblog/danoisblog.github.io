7e6091f4e80d7610118d3b659cc23e4795441536
danoisblog <danoisblog@gmail.com>
2025-10-23 14:41:35 -0400

    Update hashtags.js

--- assets/js/hashtags.js
+++ assets/js/hashtags.js
@@ -1,1 +1,17 @@
+// assets/js/hashtags.js
+document.addEventListener("DOMContentLoaded", function() {
+  const posts = document.querySelectorAll(".post p");
+  
+  posts.forEach(post => {
+    let html = post.innerHTML;
+
+    // Convertir hashtags (#ejemplo) en enlaces
+    html = html.replace(/#(\w+)/g, '<a href="/tags/$1">#$1</a>');
+
+    // Convertir URLs (https://ejemplo.com) en enlaces
+    html = html.replace(/(https?:\/\/[^\s]+)/g, '<a href="$1" target="_blank">$1</a>');
+
+    post.innerHTML = html;
+  });
+});
 
